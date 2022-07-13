0f05f5ec231b511afa913be70fff4c1b8a4b8b61
ViniciusVBS <vinyvilas@gmail.com>
2022-07-13 15:09:27 -0300

    Referencing User posts

--- src/main/java/com/educandowebvinicius/workshopmongodb/domain/User.java
+++ src/main/java/com/educandowebvinicius/workshopmongodb/domain/User.java
@@ -1,8 +1,11 @@
 package com.educandowebvinicius.workshopmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
@@ -14,6 +17,9 @@ public class User implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 	}
@@ -49,6 +55,14 @@ public class User implements Serializable{
 		this.email = email;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -72,5 +86,5 @@ public class User implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}	
+	}
 }
