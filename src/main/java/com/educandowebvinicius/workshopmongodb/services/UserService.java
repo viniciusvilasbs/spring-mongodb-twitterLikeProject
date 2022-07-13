407e04683ac87a907a1753c1d8dc19f1572f8094
ViniciusVBS <vinyvilas@gmail.com>
2022-07-13 12:16:45 -0300

    Getting an User by id

--- src/main/java/com/educandowebvinicius/workshopmongodb/services/UserService.java
+++ src/main/java/com/educandowebvinicius/workshopmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.educandowebvinicius.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandowebvinicius.workshopmongodb.domain.User;
 import com.educandowebvinicius.workshopmongodb.repository.UserRepository;
+import com.educandowebvinicius.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
