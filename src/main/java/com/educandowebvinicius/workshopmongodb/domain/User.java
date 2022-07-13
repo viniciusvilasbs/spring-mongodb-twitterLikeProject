a130cce24b14166057b4cba8417cf6a476733a82
ViniciusVBS <vinyvilas@gmail.com>
2022-07-13 11:28:51 -0300

    Conecting to MongoDB with repository and service

--- src/main/java/com/educandowebvinicius/workshopmongodb/domain/User.java
+++ src/main/java/com/educandowebvinicius/workshopmongodb/domain/User.java
@@ -2,11 +2,15 @@ package com.educandowebvinicius.workshopmongodb.domain;
 
 import java.io.Serializable;
 
-import java.io.Serializable;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+
+@Document
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private String id;
 	private String name;
 	private String email;
