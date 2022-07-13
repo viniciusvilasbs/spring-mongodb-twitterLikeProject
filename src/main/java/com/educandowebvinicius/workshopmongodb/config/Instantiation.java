d028f17d9ec5ceeb81154fcab3ea465a992b72d7
ViniciusVBS <vinyvilas@gmail.com>
2022-07-13 15:34:01 -0300

    Adding comments to posts

--- src/main/java/com/educandowebvinicius/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/educandowebvinicius/workshopmongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.educandowebvinicius.workshopmongodb.domain.Post;
 import com.educandowebvinicius.workshopmongodb.domain.User;
 import com.educandowebvinicius.workshopmongodb.dto.AuthorDTO;
+import com.educandowebvinicius.workshopmongodb.dto.CommentDTO;
 import com.educandowebvinicius.workshopmongodb.repository.PostRepository;
 import com.educandowebvinicius.workshopmongodb.repository.UserRepository;
 
@@ -40,7 +41,14 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
-
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
